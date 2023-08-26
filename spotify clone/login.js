349dac65d3930fc7f32e562fe0e51b477ac9892a
Samim Mistry <122689341+SpdySamim@users.noreply.github.com>
2023-08-27 00:01:43 +0530

    Delete spotify clone directory

--- spotify clone/login.js
+++
@@ -1,7 +0,0 @@
-let btn=document.querySelector("form");
-btn.addEventListener("submit",function(event){
-event.preventDefault();
-let usr=document.querySelector("#usrnm");
-let pss=document.querySelector("#paswrd");
-alert(`Hiii  ${usr.value} welcome to spotify`);
-});
